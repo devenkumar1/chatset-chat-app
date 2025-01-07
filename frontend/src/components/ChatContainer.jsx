0bacb86bdb0a79a7f832675137cb30600041b415
devenkumar1 <devenkumar540@gmail.com>
2025-01-07 18:29:45 +0530

    fixes

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useRef } from 'react';
 import { useChatStore } from '../store/useChatStore';
 import ChatHeader from './ChatHeader';
@@ -39,20 +38,21 @@ function ChatContainer() {
   }
 
   return (
-    <div className="flex flex-col flex-1 w-full overflow-hidden bg-gray-50">
+    <div className="flex flex-col overflow-hidden bg-gray-50 ">
       <ChatHeader />
-      <div className="flex-col flex-1 h-full space-y-6 overflow-y-auto bg-gray-100 lg:p-4 sm:2">
+      <div className="flex p-4 space-y-6 overflow-y-auto bg-gray-100">
         {messages.map((message) => (
           <div
             key={message._id}
             ref={messageEndRef}
-            className={`flex items-start lg:space-x-3 sm:space-x-1 my-1 ${message.senderId === authUser._id ? 'justify-end' : 'justify-start'
-              }`}
+            className={`flex items-start space-x-3 ${
+              message.senderId === authUser._id ? 'justify-end' : 'justify-start'
+            }`}
           >
             {/* Avatar */}
-            <div className={`lg:w-10 w-8 h-8 lg:h-10 ${message.senderId === authUser._id ? 'order-2' : ''}`}>
+            <div className={`w-10 h-10 ${message.senderId === authUser._id ? 'order-2' : ''}`}>
               <img
-                className="w-8 h-8 border border-gray-300 rounded-full lg:w-10 lg:h-10"
+                className="w-10 h-10 border border-gray-300 rounded-full"
                 src={
                   message.senderId === authUser._id
                     ? authUser.profilePic
@@ -64,8 +64,9 @@ function ChatContainer() {
 
             {/* Message Content */}
             <div
-              className={`max-w-xs px-4 py-2 text-sm text-gray-800 bg-white rounded-lg shadow-md ${message.senderId === authUser._id ? 'bg-blue-100' : 'bg-white'
-                }`}
+              className={`max-w-xs px-4 py-2 text-sm text-gray-800 bg-white rounded-lg shadow-md ${
+                message.senderId === authUser._id ? 'bg-blue-100' : 'bg-white'
+              }`}
             >
               {message.image && (
                 <div className="border border-blue-500">
@@ -85,8 +86,8 @@ function ChatContainer() {
           </div>
         ))}
       </div>
-      <MessageInput />
-
+      <div className='flex flex-wrap w-full'><MessageInput /></div>
+      
     </div>
   );
 }
